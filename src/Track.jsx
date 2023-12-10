f778480c4b61dc11c66597f3f1b3e652a108f09b
Aarush342 <149887515+Aarush342@users.noreply.github.com>
2023-12-10 20:56:27 +0000

    1.0

---
+++ src/Track.jsx
@@ -0,0 +1,12 @@
+import { useLoader } from "@react-three/fiber";
+import { useEffect } from "react";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { TextureLoader } from "three/src/loaders/TextureLoader";
+import { ColliderBox } from "./ColliderBox";
+
+export function Track() {
+  return (
+    <>
+    </>
+  );
+}
